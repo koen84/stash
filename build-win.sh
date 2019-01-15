4b8a1629f2e0e7f3114c31c5bacd98577676076a
pier <pierstabilini@gmail.com>
2019-01-14 16:38:02 -0800

    minor fixes windows build

--- build-win.sh
+++ build-win.sh
@@ -10,4 +10,4 @@ cd depends/ && make HOST=$HOST V=1 && cd ../
 ./autogen.sh
 
 CXXFLAGS="-DPTW32_STATIC_LIB -DCURVE_ALT_BN128 -fopenmp -pthread" CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site  ./configure --prefix=/  --enable-static --disable-shared  --disable-zmq --disable-rust  --disable-tests  --disable-gui-tests --disable-bench
-CC="${CC}" CXX="${CXX}" make V=1 -j3
+CC="${CC}" CXX="${CXX}" make V=1 "$@"
